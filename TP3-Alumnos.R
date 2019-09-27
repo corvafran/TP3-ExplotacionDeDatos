b0c33349b5ea1e9222bf822c105590a19d138a0d
fran <corvalanfranco@gmail.com>
2019-09-27 20:40:48 -0300

    Update vars

--- TP3-Alumnos.R
+++ TP3-Alumnos.R
@@ -22,7 +22,7 @@ View(df_alumnos)
 # Analizar los datos ingresados
 my_data_status=df_status(df_alumnos, print_results = F)
 ##Elimino filas G1, G2 y G3 que contengan 0
-View(df_alumnos[!(df_alumnos$G2==0) &!(df_alumnos$G1==0) & !(df_alumnos$G3==0) , ] )
+df_alumnos <- df_alumnos[!(df_alumnos$G2==0) &!(df_alumnos$G1==0) & !(df_alumnos$G3==0) , ] 
 ## Ordenar datos según el porcentaje de ceros
 arrange(my_data_status, -p_zeros) %>% select(variable, q_zeros, p_zeros)
 # Quitar las variables que tienen un 60% de valores cero
@@ -36,13 +36,11 @@ valoresNa<-which(is.empty(df_alumnos))
 df_alumnos.isnull().any()
 View(valoresNa)
 
-
-
 ##Elimino la variable escuela yaque es un 
 ##dato irrelevante para el analisis 
 ##como la escuela, el sexo y la edad
 ##yaque por este analisis no van a ser patrones que nos interese ver
-df_alumnos <- select(df_alumnos, -c("school", "sex", "age"))
+## df_alumnos <- select(df_alumnos, -c("school"))
 ##Fuciono el promedio de los tres trimestres
 df_alumnos$promedioTrimestres<- ((df_alumnos$G1 
                                 + df_alumnos$G2 
@@ -56,7 +54,7 @@ View(df_alumnos)
 ##y vemos que no tenemos mas valores missing(cantidad de valores faltantes)
 describe(df_alumnos)
 
-## Vericamos coolinealidad. Funciona? lo probe y no funco capaz hay que cambiar cosas o hacer otra cosa
+## Vericamos coolinealidad. 
 ggpairs(df_alumnos, lower = list(continuous = "smooth"), 
         diag = list(continuous = "bar"),
         axisLabels = "none")
@@ -75,18 +73,20 @@ modelo <- lm(promedioTrimestres ~ absences + sex + age + asesinatos +
 
 ##Convertimos las variables categoricas a dummies 
 colnames(df_alumnos)
-dummyCols <- c("sex", "Pstatus", "address", 
+dummyCols <- c("school","sex", "Pstatus", "address", 
                "famsize", "Mjob", "Fjob","reason",
                "guardian", "schoolsup","famsup", "paid", 
                "activities", "nursery", "higher",
                "internet", "romantic", "freetime")
 df_alumnos <- dummy_cols(df_alumnos,  select_columns = dummyCols)
 df_alumnos <- select(df_alumnos, -dummyCols)
+
+boxplot(df_alumnos)
 ##Una vez que tenemos las dummies hay que generar el modelo
 ##De esta forma vamos el impacto que tienen las dummies y 
 ##el incremento provocado de la variable independiente sobre la dependiente
-##modelo <- lm(esp_vida ~ habitantes + ingresos + analfabetismo + asesinatos +
-  ##             universitarios + heladas + area + densidad_pobl, data = datos )
+## modelo <- lm(esp_vida ~ habitantes + ingresos + analfabetismo + asesinatos +
+##           universitarios + heladas + area + densidad_pobl, data = datos )
 summary(modelo)
 
 round(cor(x = df_alumnos, method = "pearson"), 3)
